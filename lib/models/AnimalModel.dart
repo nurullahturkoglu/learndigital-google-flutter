a699c630037de58050df8c4ab111bf6f0b98dca5
Nurullah Türkoğlu <nurullahtrkglu@gmail.com>
2023-04-15 17:16:48 +0300

    changed animal model

--- lib/models/AnimalModel.dart
+++ lib/models/AnimalModel.dart
@@ -1,12 +1,19 @@
 class Animal {
-  String name;
-  int age;
-  String color;
-  bool haveLeg;
+  final String name;
+  final int age;
+  final String color;
+  final bool haveLeg;
 
   Animal(
       {required this.name,
       required this.age,
       required this.color,
       required this.haveLeg});
+
+  factory Animal.jsonDecode(Map<String, dynamic> map) => Animal(
+        name: map['name'],
+        age: map['age'],
+        color: map['color'],
+        haveLeg: map['haveLeg'],
+      );
 }
