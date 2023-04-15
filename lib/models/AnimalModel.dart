13ce89ea8be7e456299f022ea5ca13a3e749f161
Nurullah Türkoğlu <nurullahtrkglu@gmail.com>
2023-04-15 17:05:40 +0300

    added animal model

---
+++ lib/models/AnimalModel.dart
@@ -0,0 +1,12 @@
+class Animal {
+  String name;
+  int age;
+  String color;
+  bool haveLeg;
+
+  Animal(
+      {required this.name,
+      required this.age,
+      required this.color,
+      required this.haveLeg});
+}
