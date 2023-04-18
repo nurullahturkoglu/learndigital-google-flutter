9e4b45b4cc54f0945a3ffc77a86589f86dd6d5c9
Nurullah Türkoğlu <turkoglunurullah99@gmail.com>
2023-04-18 19:13:20 +0300

    register & email send verification

---
+++ lib/services/firebase_auth_methods.dart
@@ -0,0 +1,39 @@
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:myapp/widgets/snackbars.dart';
+
+class FirebaseAuthMethods {
+  final FirebaseAuth _auth;
+  FirebaseAuthMethods(this._auth);
+
+  // EMAIL Sign Up
+  Future<void> signUpWithEmail({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      await sendMailVerification(context);
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message!, ContentType.failure);
+    }
+  }
+
+  sendMailVerification(BuildContext context) async {
+    try {
+      _auth.currentUser!.sendEmailVerification();
+      showSnackBar(
+        context,
+        'Email verification send.',
+        ContentType.success,
+      );
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message!, ContentType.failure);
+    }
+  }
+}
